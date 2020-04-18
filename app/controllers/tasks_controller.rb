e514ab3ea3f19496e7a53661f42ac0f4799591e4
YusukeMancity <yusuke1548@gmail.com>
2020-04-18 22:39:41 +0900

    編集＆削除機能の追加、レイアウトの調整

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,12 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
-    @task = Task.new(task: params[:task])
+    @task = Task.new
   end
 
   def create
@@ -12,11 +16,25 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end 
+
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
-
+  
+  #StrongParametersによるセキュリティ対策（想定外のデータを受け取らないようにするもの）
   def task_params
     params.require(:task).permit(:task)
   end
